5520ee0627b171c0872330c2c09b27ba918f6e1f
Riza Hariati <76550345+RizaHariati@users.noreply.github.com>
2023-07-05 08:29:29 +0700

    membuat custom not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const CustomNotFound = (props: Props) => {
+  return <div>CustomNotFound</div>;
+};
+
+export default CustomNotFound;
+
+// Not found harus berada di folder app
